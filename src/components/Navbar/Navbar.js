1bf8cf3dc9bce87950cd053a0d22eadfde1b0cab
SrushGit <srupatil2302@gmail.com>
2024-10-05 16:13:31 +0530

    successfull

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,22 +3,22 @@ import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/data">Facebook</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container-fluid">
+        <a className="navbar-brand" href="/data">Facebook</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto my-auto">
-            <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="/data">Home</a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto my-auto">
+            <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="/data">Home</a>
             </li>
-            <li class="nav-item">
-            <a class="nav-link" href="/data">About</a>
+            <li className="nav-item">
+            <a className="nav-link" href="/data">About</a>
             </li>
-            <li class="nav-item">
-            <a class="nav-link" href="/data">Contact</a>
+            <li className="nav-item">
+            <a className="nav-link" href="/data">Contact</a>
             </li>
         </ul>
         </div>
