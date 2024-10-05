949cb0fa9531f82a2522dc4fcca8b3b926b1be9e
SrushGit <srupatil2302@gmail.com>
2024-10-05 11:00:00 +0530

    successed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,17 +5,20 @@ export default function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">Facebook</a>
+        <a class="navbar-brand" href="/data">Facebook</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav me-auto my-auto">
             <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="/">Home</a>
+            <a class="nav-link" aria-current="page" href="/data">Home</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="/">Link</a>
+            <a class="nav-link" href="/data">About</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/data">Contact</a>
             </li>
         </ul>
         </div>
